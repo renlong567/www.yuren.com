0fc16ab8d0c366253d31aa3b8feec26c7cbc8c6d
renlong567 <renlong567@gmail.com>
2015-08-03 17:04:47 +0800

    关闭手机端检测

--- config.php
+++ config.php
@@ -22,8 +22,8 @@ $config['db']['cache']['port'] = 11211;
 $config['db']['cache']['time'] = 86400; //Memcache限制不能超过30天，我们建议设置86400，一天
 
 //手机端配置
-$config['mobile']['autocheck'] = true; //自动检测手机端，启用后，检测出手机端将读取手机端网页
-$config['mobile']['status'] = true; //手机端开始，此项不开启的话，将不使用手机
+$config['mobile']['autocheck'] = false; //自动检测手机端，启用后，检测出手机端将读取手机端网页
+$config['mobile']['status'] = false; //手机端开始，此项不开启的话，将不使用手机
 $config['mobile']['default'] = false; //默认为手机版，为方便开发人员调式，设置为默认后，在网页上也会展示手机版
 
 $config['develop'] = true; //开发模式，正常运行的网站请设为false，可防止CRSF注入
